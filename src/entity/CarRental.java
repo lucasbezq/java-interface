f1d8a678c6c33ea6f6091265dfba9bdf0c4bb71a
Lucas Barros <lucasbarrosez@gmail.com>
2021-02-02 14:29:15 -0300

    implemented domain layer

---
+++ src/entity/CarRental.java
@@ -0,0 +1,53 @@
+package entity;
+
+import java.util.Date;
+
+public class CarRental {
+
+	private Date start;
+	private Date finish;
+
+	private Vehicle vechicle;
+	private Invoice invoice;
+
+	public CarRental() {
+	}
+
+	public CarRental(Date start, Date finish, Vehicle vechicle) {
+		this.start = start;
+		this.finish = finish;
+		this.vechicle = vechicle;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getFinish() {
+		return finish;
+	}
+
+	public void setFinish(Date finish) {
+		this.finish = finish;
+	}
+
+	public Vehicle getVechicle() {
+		return vechicle;
+	}
+
+	public void setVechicle(Vehicle vechicle) {
+		this.vechicle = vechicle;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+}
